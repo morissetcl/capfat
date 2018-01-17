3f236960fa242af12b7c24054ed37f95017a9eaa
Clement Morisset <morissetcl87@gmail.com>
2018-01-17 08:31:33 +0100

    ajoute shared_example

--- spec/features/artworks/index_spec.rb
+++ spec/features/artworks/index_spec.rb
@@ -3,10 +3,5 @@ require "rails_helper"
 RSpec.describe "Artwork#index", :feature do
 
   include_context 'login'
-
-  it 'affiche la liste des oeuvres', :focus do
-    artwork = create :artwork
-    visit artworks_path
-    expect(page).to artwork_index_page(artwork)
-  end
+  it_behaves_like 'artwork_index_page', :artwork
 end
