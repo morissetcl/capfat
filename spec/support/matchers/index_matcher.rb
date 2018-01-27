ad73f88d7f0b2b7e586f6b2b2e6186e949027487
Clement Morisset <morissetcl87@gmail.com>
2018-01-27 09:03:58 +0100

    ajuote test

---
+++ spec/support/matchers/index_matcher.rb
@@ -0,0 +1,11 @@
+RSpec::Matchers.define :artwork_index_page do |model|
+  it 'affiche la liste des oeuvres' do
+    resource = create model
+    path = model.to_s
+    visit("/#{path}s")
+    expect(page).to have_content resource.name
+    expect(page).to have_content resource.description
+    expect(page).to have_content resource.description
+  end
+
+end
