4e043bc46d3e6c1df3c27362c10623da0ae9eaf9
Clement Morisset <morissetcl87@gmail.com>
2018-02-04 12:46:49 +0100

    allow to add user

---
+++ db/migrate/20180204113221_add_adress_to_users.rb
@@ -0,0 +1,7 @@
+class AddAdressToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :street, :string
+    add_column :users, :city, :string
+    add_column :users, :zip_code, :string
+  end
+end
