9aff37bc4632bdca0319b806c386b74d402339c5
Clement Morisset <morissetcl87@gmail.com>
2018-02-11 10:58:15 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  root to: 'home#index'
+  root to: 'artworks#index'
   resources :artworks
 end
